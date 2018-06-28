e6e0c863061c0c90db30f2ae4ca0fa440e45b6c2
adam mcbride <adam.mcbride@gmail.com>
2018-06-28 13:51:56 -0400

    more layouts complete. xcode is getting crashy, so i'm pushing commits more frequently, just in case

---
+++ ThinkUp DrinkUp/ThinkUp DrinkUp/bigButton.swift
@@ -0,0 +1,30 @@
+//
+//  blueButton.swift
+//  ThinkUp DrinkUp
+//
+//  Created by Adam McBride on 6/27/18.
+//  Copyright © 2018 Adam McBride Studios. All rights reserved.
+//
+
+import UIKit
+
+class bigButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // adds a white border
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+        
+        // adds rounded corners
+        layer.cornerRadius = 8
+        
+        // adds a drop shadow
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 0)
+        layer.shadowRadius = 2
+        layer.shadowOpacity = 0.2
+    }
+
+}
---
+++ ThinkUp DrinkUp/ThinkUp DrinkUp/playerLabel.swift
@@ -0,0 +1,21 @@
+//
+//  playerLabel.swift
+//  ThinkUp DrinkUp
+//
+//  Created by Adam McBride on 6/28/18.
+//  Copyright © 2018 Adam McBride Studios. All rights reserved.
+//
+
+import UIKit
+
+class playerLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
