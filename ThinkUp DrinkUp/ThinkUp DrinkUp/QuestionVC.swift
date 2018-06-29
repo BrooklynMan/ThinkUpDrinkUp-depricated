d5b8f242181fe0ee2d3d9c45bbffcf19b998daac
adam mcbride <adam.mcbride@gmail.com>
2018-06-29 19:40:22 -0400

    Code complete. I figured out what was wrong-- when I first coded this app 2 years ago, I coded it for shit. It's all set up wrong, and so the values for currentSubject weren't being updated because how i've arranged how everything is stored and called is, well, wrong. to fix it, i'd have to re-write most of the code. I'm not going to do that now, as I just want to push a working app. I may do that for a version 2, if i ever update this app. otherwise, the app works just fine as-is.

--- ThinkUp DrinkUp/ThinkUp DrinkUp/QuestionVC.swift
+++ ThinkUp DrinkUp/ThinkUp DrinkUp/QuestionVC.swift
@@ -58,5 +58,35 @@ class QuestionVC: UIViewController {
         let thisQuestionNumber = String(questionNumber + 1)
         QuestionAndRoundLabel.text = "Question: " + thisQuestionNumber + "Round: " + thisRoundNumber
     }
-
+    
+    // questions array
+    
+    var questions:[String] = [
+        "" + currentSubject + " is given a ridiculous superpower. What is it?",
+        "If " + currentSubject + " won the lottery, what would be the first thing they would buy?",
+        "What is " + currentSubject + "'s secret weapon?",
+        "" + currentSubject + " is on Jeopardy!, but all the categories are ridiculous subjects. What’s his/her best-performing category?",
+        "What do you think is " + currentSubject + "’s greatest fear?",
+        "Was " + currentSubject + " a ‘licker’, a ‘biter’, or a ‘picker’ as a child?",
+        "What do you think is " + currentSubject + "’s best skill or talent?",
+        "If " + currentSubject + " was going to have sex in public, where would it be?",
+        "What is the most embarrassing thing you know about " + currentSubject + "?",
+        "What is the most inappropriate thing you’ve seen " + currentSubject + " do?",
+        "At what type of competition would " + currentSubject + " excel?",
+        "What fictional character is " + currentSubject + " most like?",
+        "You and " + currentSubject + " decide to skip school for the day. What do you go do?",
+        "If " + currentSubject + " were a Bond villain, which one would they be?",
+        "If you were a waiter and " + currentSubject + " was rude to you, what would you do to their food?",
+        "You and " + currentSubject + " get pulled over, and the cop finds a bag of weed in the center console. Do you own own up to it, or do you blame it on " + currentSubject + "?",
+        "What terrible band do you think " + currentSubject + " secretly likes?",
+        "If " + currentSubject + " was a Game of Thrones character, which one would they be?",
+        "" + currentSubject + ": Star Trek or Star Wars?",
+        "What is " + currentSubject + "'s disgusting guilty pleasure?",
+        "If " + currentSubject + " was at a TSA checkpoint and found they had a small bag of weed in their pocket right before going through the X-Ray machine, what would they do?",
+        "What its " + currentSubject + "’s secret fetish?",
+        "You and " + currentSubject + " are stranded on a desert island. You find a huge coconut. Do you share it or hoard it?",
+        "" + currentSubject + " accidentally texts a nude pic to his/her boss. What's their next text?",
+        "The Joker has kidnapped " + currentSubject + ". Which Batman would you want to save him/her? West, Keaton, Kilmer, Clooney, Bale, or Affleck?"
+    ]
+    
 }
